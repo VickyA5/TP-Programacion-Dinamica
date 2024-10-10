6f39979417dddbb6910378a8694d04aabe7524ae
Santiago Tissoni <santi.tissoni@gmail.com>
2024-10-10 17:04:14 -0300

    Problema .py

--- problema.py
+++ problema.py
@@ -1,14 +1,16 @@
-'''
+"""
 Ecuacion de recurrencia:
 
-OPT(i, j) = max( coins[i] + min( OPT(i+2, j), OPT(i+1, j-1) ),   Agarrar izquierda
-                coins[j] + min( OPT(i, j-2), OPT(i+1, j-1) ) )   Agarrar derecha
-'''
+OPT(i, j) = max( 
+    coins[i] + min( OPT(i+2, j), OPT(i+1, j-1) ),  Agarrar izquierda
+    coins[j] + min( OPT(i, j-2), OPT(i+1, j-1) )   Agarrar derecha
+)
+"""
 
-import os
 import sys
 
 # FALTA LA RECONSTRUCCION
+
 
 def valor_max_sophia(coins):
     n = len(coins)
@@ -38,37 +40,9 @@ def valor_max_sophia(coins):
     return F
 
 
-def tests(file_path):
-    output_folder = "outputs"
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
-    try:
-        with open(file_path, "r") as file:
-            first_line = file.readline().strip()
-            if first_line.startswith("#"):
-                fila = list(int(i) for i in file.readline().strip().split(";") if i)
-            else:
-                fila = list(int(i) for i in first_line.split(";") if i)
-            
-            F = valor_max_sophia(fila)
-            valor_sophia = F[0][len(fila) - 1]
-
-            print(f"Corriendo algoritmo con el contenido del archivo {file_path}\n")
-            print(f"Ganancia Sophia: {valor_sophia}\n")
-
-            # output_filename = os.path.basename(file_path).replace(".txt", "_resultado.txt")
-            # with open(f"{output_folder}/{output_filename}", "w") as output_file:
-            #   output_file.write(f"Esperados: {'; '.join(map(str, esperados))}\n")
-            # print(f"\nLos resultados de las acciones individuales de cada jugador se encuentran en outputs/{output_filename}.\n")
-    except Exception as e:
-        print(f"Error al procesar el archivo {file_path}: {e}")
-
-        
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Argumentos incorrectos.\n" +
-              "Para ejecutar el algoritmo con un txt: python3 problema.py <archivo_set_de_datos.txt>\n")
-    else:
-        file_path = sys.argv[1]
-        tests(file_path)
+    coins = sys.argv[1]
+    coins = list(map(int, coins.split(";")))
+    F = valor_max_sophia(coins)
+    valor_sophia = F[0][len(coins) - 1]
+    print(f"Ganancia Sophia: {valor_sophia}")
