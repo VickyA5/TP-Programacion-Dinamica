40e7cfe20b33bbb00858641f8b7dd4c260c417ef
VickyA5 <vavalos@fi.uba.ar>
2024-11-21 20:37:23 -0300

    changes in factor aproximacion

--- factor_aproximacion.py
+++ factor_aproximacion.py
@@ -2,37 +2,37 @@ from problema_backtracking import backtracking
 from aproximacion import aproximar
 from pruebas import open_file
 import sys
-import copy
 
 
 def calculate_approximation_factor(filename):
     path = "./test_cases/" + filename
-    row_demand, col_demand, ships = open_file(path)
 
+    row_demand, col_demand, ships = open_file(path)
     n = len(row_demand)
     m = len(col_demand)
 
     total_demanda = sum(row_demand) + sum(col_demand)
-    row_demand_inicial = copy.deepcopy(row_demand)
-    col_demand_inicial = copy.deepcopy(col_demand)
+    row_demand_inicial = row_demand.copy()
+    col_demand_inicial = col_demand.copy()
 
     board = [[0] * m for _ in range(n)]
-    board_original = copy.deepcopy(board)
+    board_inicial = board.copy()
     barcos = [(i + 1, length) for i, length in enumerate(ships)]
-    barcos_original = copy.deepcopy(barcos)
-    mejor_solucion = [copy.deepcopy(board), float("inf")]
+    barcos_inicial = barcos.copy()
+    mejor_solucion = [board, float("inf")]
+
+    demanda_cumplida_aproximacion = aproximar(row_demand_inicial, col_demand_inicial, barcos, board)[1]
 
     backtracking(
-        board,
-        barcos,
-        row_demand,
-        col_demand,
+        board_inicial,
+        barcos_inicial,
+        row_demand_inicial,
+        col_demand_inicial,
         mejor_solucion
     )
 
     demanda_cumplida_backtracking = total_demanda - mejor_solucion[1]
-    demanda_cumplida_aproximacion = aproximar(row_demand_inicial, col_demand_inicial, barcos_original, board_original)[1]
-        
+       
     factor = demanda_cumplida_aproximacion / demanda_cumplida_backtracking
     return factor
 
